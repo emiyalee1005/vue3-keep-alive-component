311e0500dfa9511bc5bea14fb4e0625a58eacc4c
EmiyaLee <2288136aaa@gmail.com>
2023-10-09 23:48:20 +0800

    Support native keepalive props

--- src/components/KeepAliveComponent/KeepAliveComponent.vue
+++ src/components/KeepAliveComponent/KeepAliveComponent.vue
@@ -1,14 +1,26 @@
 <template>
-  <keep-alive>
+  <keep-alive v-bind="keepAliveProps">
     <component :is="isComponent" />
   </keep-alive>
 </template>
 <script lang="ts" setup>
-import { onActivated, onDeactivated, shallowRef, watch } from 'vue'
+import { computed, onActivated, onDeactivated, shallowRef, watch } from 'vue'
+type MatchPattern = string | RegExp | (string | RegExp)[]
 
-const props = defineProps<{ component?: any }>()
+const props = defineProps<{
+  component?: any
+  include?: MatchPattern
+  exclude?: MatchPattern
+  max?: number | string
+}>()
 
 const isActivated = shallowRef(true)
+
+const keepAliveProps = computed(() => {
+  const _props = { ...props }
+  delete _props['component']
+  return _props
+})
 
 onActivated(() => {
   isActivated.value = true
